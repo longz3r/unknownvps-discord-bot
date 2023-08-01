6975e6894f8671668f82a6a104727e77444142a8
longz3r <gialongminecraft@gmail.com>
2023-08-01 21:02:37 +0700

    createvps.js

--- commands/createvps.js
+++ commands/createvps.js
@@ -7,23 +7,22 @@ async function createVPS(interaction) {
         "discordId": interaction.user.id,
         "plan": plan
     }
-    interaction.reply("ngu")
-    // res = await sendAPIRequest("/", "POST", {}, requestBody)
+    res = await sendAPIRequest("/vps/assign", "POST", {}, requestBody)
     
-    // if (res.status == 200) {
-    //     console.log(`${interaction.user.tag} created VPS with plan ${plan}`)
-    //     interaction.reply(`You created VPS with ${plan} plan successfully`)
-    // } else {
-    //     interaction.reply(`Something went wrong\nMessage: ${res.data}\n This is not your fault, we will investigate it`)
-    //     console.log(`/////////////////////////////////////////////\n
-    //     ERROR\n
-    //     AT CREATE VPS\n
-    //     DISCORD ID ${interaction.user.id}\n
-    //     PLAN ${plan}\n
-    //     API RESPONSE: ${res.status}\n
-    //     ${res.data}\n
-    //     /////////////////////////////////////////////`)
-    // }
+    if (res.status == 200) {
+        console.log(`${interaction.user.tag} created VPS with plan ${plan}`)
+        interaction.reply(`You created VPS with ${plan} plan successfully`)
+    } else {
+        interaction.reply(`Something went wrong\nMessage: ${res.data}`)
+        console.log(`/////////////////////////////////////////////\n
+        ERROR\n
+        AT CREATE VPS\n
+        DISCORD ID ${interaction.user.id}\n
+        PLAN ${plan}\n
+        API RESPONSE: ${res.status}\n
+        ${res.data}\n
+        /////////////////////////////////////////////`)
+    }
 }
 
 module.exports = createVPS
